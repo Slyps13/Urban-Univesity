e28bbc0a8ece65f238b9f6f529d026733aab3a97
Slyps13 <Slyps13@gmail.com>
2024-07-25 10:51:09 +0400

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,1 +1,12 @@
-
+def  print_params(a = 1, b = 'строка', c = True) :
+    print(a,b,c)
+print_params()
+print_params(2,'string')
+print_params(b = 25)
+print_params(c = [1,2,3])
+values_list = [495,'Мосва',7373948]
+values_dict = { 'a':495 ,'b':'Moskow', 'c': True}
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list )
+print_params(**values_dict)
+print_params(*values_list_2, 42)
